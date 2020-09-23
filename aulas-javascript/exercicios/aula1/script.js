ecdef893bd70fc2a854034ce9f865c470862d420
Natalia Andrade <naty17and@gmail.com>
2020-09-23 20:49:41 -0300

    Resolucao execicio 1 aula de javascript Natalia Andrade

---
+++ aulas-javascript/exercicios/aula1/script.js
@@ -0,0 +1,8 @@
+function checarSePodeDirigir(){
+    let idade = prompt('Digite a idade ');
+    if (idade>18){
+        alert('Já Pode Dirigir!');
+    }else
+    alert('Ainda não pode Dirigir!');
+}
+checarSePodeDirigir()
